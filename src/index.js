12a87939d94762deef55a9997da30c9e89f84365
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-27 16:54:02 -0400

    add login, signup and logout functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./pages/App/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route component={App} />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
