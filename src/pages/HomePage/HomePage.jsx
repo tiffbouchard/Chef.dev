d991f9f260b5cb023c7536312f8d8f29d3eea58b
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-28 01:14:53 -0400

    add sidebar and feed areas with grid

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import './HomePage.css';
-import Feed from '../../components/Feed/Feed';
-import MainSidebar from '../../components/MainSidebar/MainSidebar';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import Feed from "../../components/Feed/Feed";
+import MainSidebar from "../../components/MainSidebar/MainSidebar";
+// import { Grid } from "@material-ui/core";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  sidebar: {
+    backgroundColor: "grey",
+  },
+}));
 
 const HomePage = (props) => {
+  const classes = useStyles();
 
-    return (
-        <div>
-
-            <Grid backgroudColor='red' container spacing={12}>
-                <Grid container spacing={8}>
-                    <Feed />
-                </Grid>
-                <Grid container spacing={4}>
-                    <MainSidebar />
-                </Grid>
-            </Grid>
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={9} sm={9} md={9} component={Paper}>
+        <div className={classes.paper}>
+          <Feed />
         </div>
-    )
-}
+      </Grid>
+      <Grid item xs={3} sm={3} md={3} className={classes.sidebar}>
+        <MainSidebar />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default HomePage;
