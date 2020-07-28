845d433308a9fd56e8611d93432f2b438a12bedc
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-28 01:14:16 -0400

    style signup page

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,6 +1,39 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import profileService from "../../utils/profileService";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 
 class SignupForm extends Component {
   state = {
@@ -45,58 +78,141 @@ class SignupForm extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <header>Sign Up</header>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="First Name"
-            value={this.state.firstName}
-            name="firstName"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            value={this.state.lastName}
-            name="lastName"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            placeholder="Username"
-            value={this.state.username}
-            name="username"
-            onChange={this.handleChange}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={this.state.email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            name="password"
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={this.state.passwordConf}
-            name="passwordConf"
-            onChange={this.handleChange}
-          />
-          <button disabled={this.isFormInvalid()}>Sign Up</button>
-          {/* <Link to="/">Cancel</Link> */}
-        </form>
-      </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} onSubmit={this.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  type="text"
+                  placeholder="First Name"
+                  value={this.state.firstName}
+                  name="firstName"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                  type="text"
+                  placeholder="Last Name"
+                  value={this.state.lastName}
+                  name="lastName"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  type="text"
+                  placeholder="Username"
+                  value={this.state.username}
+                  name="username"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  type="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  name="email"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  name="password"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={this.state.passwordConf}
+                  name="passwordConf"
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Button
+                disabled={this.isFormInvalid()}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
     );
   }
 }
 
-export default SignupForm;
+export default withStyles(styles)(SignupForm);
