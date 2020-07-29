9ac12806a63d1bce58c50855cb825eb837f28fbf
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-29 14:11:39 -0400

    merge conflict

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -7,8 +7,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles } from "@material-ui/core/styles";
-import Footer from "../../components/Footer/Footer"
-
+import Footer from "../../components/Footer/Footer";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,28 +26,6 @@ const useStyles = makeStyles((theme) => ({
 
 //comp did mount mathc.params.....
 
-<<<<<<< HEAD
-        <Grid container component="main" className={classes.root}>
-            <CssBaseline />
-            <Grid item xs={9} sm={9} md={9} component={Paper}>
-                <div className={classes.paper}>
-                    <Content />
-                </div>
-            </Grid>
-            <Grid item xs={3} sm={3} md={3} className={classes.sidebar}>
-                <DetailSidebar />
-            </Grid>
-            <Grid item xs={9} sm={9} md={9} component={Paper}>
-                <div className={classes.paper}>
-                    <Tips />
-                </div>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} component={Paper}>
-                <Footer />
-            </Grid>
-        </Grid>
-    );
-=======
 const DetailPage = (props) => {
   const classes = useStyles();
   return (
@@ -69,7 +46,6 @@ const DetailPage = (props) => {
       </Grid>
     </Grid>
   );
->>>>>>> 61ab6a53c889a9a0fc4a29aa489620b75c8d83bf
 };
 
 export default DetailPage;
