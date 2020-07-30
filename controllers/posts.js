44a2f812b61351f3782388e24f3952740311ab71
mohamed <mohamed.aden@live.ca>
2020-07-30 15:16:23 -0400

    Post delete and view funcs

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,41 +2,63 @@ const Post = require("../models/post");
 const Profile = require("../models/profile");
 
 module.exports = {
-  create,
-  getPosts,
-  getPost,
+    create,
+    getPosts,
+    getPost,
+    deletePost
 };
 
 // when create post query the Profile
 async function create(req, res) {
-  const post = new Post(req.body);
-  console.log(req.body);
-  try {
-    await post.markModified("ingredients");
-    await post.save();
-    res.json({ post });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+    const post = new Post(req.body);
+    try {
+        await post.markModified("ingredients");
+        await post.save();
+        res.json({ post });
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 async function getPosts(req, res) {
-  const allPosts = await Post.find().populate("profile");
-  res.json(allPosts);
-  // add cursore() here later when lots of posts so not everything is loaded at once
-  // console.log(allPosts);
+    const allPosts = await Post.find().populate("profile");
+    res.json(allPosts);
+    // add cursore() here later when lots of posts so not everything is loaded at once
+    // console.log(allPosts);
 }
 
 function getPost(req, res) {
-  const id = req.params.id;
-  console.log(id);
-  return Post.findById(id, function (err, docs) {
-    if (err) return err;
-    console.log(docs);
-    return res.json(docs);
-  }).populate("profile");
+    const id = req.params.id;
+    // console.log("User Profile, fetching post from server to display", id);
+    return Post.findById(id, function(err, docs) {
+        if (err) return err;
+        console.log(docs);
+        return res.json(docs);
+    }).populate("profile");
 }
 
+async function deletePost(req, res) {
+    const id = req.params.id;
+    console.log("User Profile, fetching post id from server to delete", id);
+    await Post.findByIdAndDelete(id, function(err) {
+        if (err) console.log(err);
+        console.log("Successfully Deleted Post")
+    })
+}
+
+async function deletePost(req, res, next) {
+    const id = req.params.id;
+    console.log('test')
+    try {
+        Post.findByIdAndDelete(id, function(err, docs) {
+            if (err) console.log(err);
+            return res.json(docs);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 // add cursore() here later when lots of posts so not everything is loaded at once
 // console.log(allPosts);
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,8 +6,9 @@ const postCtrl = require("../../controllers/posts");
 /*---------- Public Routes ----------*/
 router.post("/new", postCtrl.create);
 router.get("/all", postCtrl.getPosts);
+// router.get("/delete/:id", postCtrl.deletePost);
 router.get("/:id", postCtrl.getPost);
-
+router.delete("/delete/:id", postCtrl.deletePost);
 /*---------- Protected Routes ----------*/
 
 module.exports = router;
